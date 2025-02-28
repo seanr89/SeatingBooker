8bf7373153d24733045828063db5bb48f1542222
seanr89 <srafferty89@gmail.com>
2025-02-28 18:29:17 +0000

    Add initial Azure App Host setup with configuration files and logging; include next-steps documentation for deployment and troubleshooting

---
+++ src/aspire-base/aspire-azure.AppHost/Program.cs
@@ -0,0 +1,16 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// var username = builder.AddParameter("username", secret: true);
+// var password = builder.AddParameter("password", secret: true);
+
+var postgres = builder.AddPostgres("postgres")
+    .PublishAsAzurePostgresFlexibleServer();
+
+// postgres.WithPgAdmin(c => c.WithHostPort(5050).WaitFor(postgres));
+var seatDb = postgres.AddDatabase("bookings");
+
+builder.AddProject<Projects.SeatingAPI>("SeatingAPI")
+    .WithExternalHttpEndpoints()
+    .WithReference(seatDb).WaitFor(postgres);
+
+builder.Build().Run();
