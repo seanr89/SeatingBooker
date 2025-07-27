d05a0697062a71e83c39d03c3d9772293a56f9e2
seanr89 <srafferty89@gmail.com>
2025-07-27 09:59:45 +0100

    more small changes

--- src/aspire-base/SeatingAPI/Entities/BookingRequest.cs
+++ src/aspire-base/SeatingAPI/Entities/BookingRequest.cs
@@ -8,8 +8,8 @@ public class BookingRequest : AuditableEntity
     public int Id { get; set; }
     public DateTime RequestDate { get; set; }
     public int DeskId { get; set; }
-    public required Desk Desk { get; set; }
+    public Desk Desk { get; set; }
     public int StaffId { get; set; }
-    public required Staff Staff { get; set; }
+    public Staff Staff { get; set; }
     public RequestState State { get; set; }
 }
